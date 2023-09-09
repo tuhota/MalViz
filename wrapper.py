9ec5dd8ae292ef699a3caba0a11f5530d83d19b9
tuhota <96323290+tuhota@users.noreply.github.com>
2023-09-08 22:20:47 -0400

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,32 +1,35 @@
 import sys
 
-main():
+
+
+def main():
+    #help/info
     if sys.argv[1] == "-h":
-    	print("Usage: wrapper.py filetype visualisation path_to_file\n Filetypes: -pe -pcap \n Visualisations: -wordcloud -other")
+    	print("Usage: wrapper.py filetype visualisation path_to_file\n Filetypes: -pe -pcap \n Visualisations: -wordcloud -most-common")
     	sys.exit()
     
     #error if too many/ few arguments
     if len(sys.argv) != 4:
     	print("Incorrect usage: use wrapper.py -h for help")
-    
+
+    #define variables from cl arguments
     filetype = str(sys.argv[1])
     visualisation = str(sys.argv[2])
     path_to_file = str(sys.argv[3])
-    
-    if (filetype == "-pe"):
-        if (visualisation == "-wordcloud"):
-            import pe_wordcloud
-            pe_wordcloud.main(path_to_file)
-    
-        elif (visualisation == "-other"):
-            pass
 
-    if (filetype == "-pcap"):
-            pass
+    #call scripts based on filetype and visualisation selected
+    if (filetype == "-pe"):
+        import pe_functions
+        pe_functions.main(path_to_file, visualisation)
+
+    elif (filetype == "-pcap"):
+            return
             
     else: 
         print("Incorrect usage: use wrapper.py -h for help")
         sys.exit()
+
+
 
 if __name__ == "__main__":
     main()
